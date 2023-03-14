dcc0f2632f8011c00c2e7385f7c05a5107baf160
Pratik Chandra <pratik.offcwrk@gmail.com>
2023-03-14 12:09:55 +0530

    Update assignment_1.groovy

--- assignment_1.groovy
+++ assignment_1.groovy
@@ -1,26 +1,39 @@
 pipeline {
-    agent any
+    options {
+        timestamps()
+    }
+
+    agent any //run this pipeline on any available agent ..... what to use here and how to know it
 
     stages {
-        stage('Build') {
+        stage('Init') {
             steps {
-                echo 'Build World'
+                echo "Initializing Pipeline"
+            }
+        } // End of 'Init'
+
+        stage('Call pipeline_1') {
+            steps {
+                script {
+                    try {
+                        def self_service_build = build(job: "TestPipeline1",
+                            propagate: true,
+                            wait: true,
+                            parameters: [
+                                [$class: 'StringParameterValue', name: 'TEST_NAME', value: "develop"],
+                            ])
+                    } catch (Exception e) {
+                        'error ("FATAL:: Ran into an issue while Running job. Error: " + e.message)'
+                    }
+                }
+        } // Do we only mean build a downstream job/pipeline when we say call another pieline ?
+    }
+	    
+    post {
+        cleanup {
+            script {
+                sh ("sudo rm -rf *.*")
             }
         }
-		stage('Test') {
-            steps {
-                echo 'Test World'
-            }
-        }
-		stage('Deploy') {
-            steps {
-                echo 'Deploy World'
-            }
-        }
-		stage('Build Hello-World') {
-			steps {
-				build 'Hello-World'
-			}
-		}
     }
 }
