23000b62d29338b004c188fe925d1ea5af88efd2
Pratik Chandra <pratik.offcwrk@gmail.com>
2023-03-21 22:46:16 +0530

    Update assignment_2.groovy

--- assignment_2.groovy
+++ assignment_2.groovy
@@ -19,7 +19,8 @@ pipeline {
                 sh "mkdir -p output"
 
                 // Write an useful file, which is needed to be archived.
-                writeFile file: "output/usefulfile.txt", text: '''This file is useful, need to archive it.\nunix is great os.\nunix was developed in Bell labs.\nlearn operating system.\nUnix linux which one you choose.\nuNix is easy to learn.unix is a multiuser os.Learn unix .unix is a powerful.'''
+                writeFile file: "output/usefulfile.txt", text: '''This file is useful, need to archive it.\nunix is great os.\nunix was developed in Bell labs.\nlearn operating system.\nUnix linux which one you choose.\nuNix is easy to learn.unix is a multiuser os.Learn unix .unix is a powerful.\nRevpro URL           : revpro-m-d202303202307.revpro.cloud
+'''
 
                 // Write an useless file, which is not needed to be archived.
                 writeFile file: "output/uselessfile.md", text: "This file is useless, no need to archive it."
