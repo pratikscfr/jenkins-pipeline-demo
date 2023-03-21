7bc5d132aaa92825cd9bc60b0a87931a5740a769
Pratik Chandra <pratik.offcwrk@gmail.com>
2023-03-21 16:39:53 +0530

    Create assignment_2.groovy

---
+++ assignment_2.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    options {
+        timestamps()
+    }
+
+    agent any
+
+    stages {
+
+        stage('Init') {
+            steps {
+                echo 'Init'
+            }
+        }
+
+        stage('Create build output') {
+            steps {
+                // Make the output directory.
+                sh "mkdir -p output"
+
+                // Write an useful file, which is needed to be archived.
+                writeFile file: "output/usefulfile.txt", text: "This file is useful, need to archive it."
+
+                // Write an useless file, which is not needed to be archived.
+                writeFile file: "output/uselessfile.md", text: "This file is useless, no need to archive it."
+            }
+        }
+
+        stage('Archive build output') {
+            steps {
+                // Archive the build output artifacts.
+                archiveArtifacts artifacts: 'output/*.txt', excludes: 'output/*.md'
+            }
+        }
+    }
+}
