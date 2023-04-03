b60199e5b7354c25999198750fc69649db1e70bd
Pratik Chandra <pratik.offcwrk@gmail.com>
2023-04-04 01:25:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
 import sys
+import argparse
 
-print("Hello World")
+def parse_args():
+    """
+    Parse Arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--URL', type=str, default='',
+                        help='The REVRPO_URL.')
+    return parser.parse_args()
 
-revProURL = "https://" + sys.argv[1] + "/#/account/login"
-
-print(revProURL)
 
 
-assert 5 == 3+2, "Wrong Addition"
+if __name__ == '__main__':
+    ARGS = parse_args()
+    print("Hello World")
+    revProURL = "https://" + ARGS.URL + "/#/account/login"
+    print(revProURL)
+    assert 5 == 3+2, "Wrong Addition"
