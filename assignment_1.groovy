25b4100cb106a58bd56359fed6e567b8ec426617
Pratik Chandra <pratik.offcwrk@gmail.com>
2023-04-04 01:41:42 +0530

    Update assignment_1.groovy

--- assignment_1.groovy
+++ assignment_1.groovy
@@ -43,7 +43,7 @@ pipeline {
                     try {
                         def REVPRO_URL = "revpro-m-d202303202307.revpro.cloud"
                         echo "${REVPRO_URL}"
-                        sh('python3 test.py --URL $REVPRO_URL')
+                        sh("python3 test.py --URL \"${REVPRO_URL}\" ")
                     } catch (Exception e) {
                         error ("Fatal: " + e.message)
                     }
